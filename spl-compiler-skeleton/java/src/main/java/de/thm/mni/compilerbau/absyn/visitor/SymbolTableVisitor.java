b53d2e020130655f02a297dd473fb7e98ed24a72
anasaji22 <anasaji55@gmail.com>
2024-06-18 23:48:40 +0200

    The end of

---
+++ spl-compiler-skeleton/java/src/main/java/de/thm/mni/compilerbau/absyn/visitor/SymbolTableVisitor.java
@@ -0,0 +1,135 @@
+package de.thm.mni.compilerbau.absyn.visitor;
+
+import de.thm.mni.compilerbau.CommandLineOptions;
+import de.thm.mni.compilerbau.absyn.*;
+import de.thm.mni.compilerbau.table.SymbolTable;
+import de.thm.mni.compilerbau.table.*;
+import de.thm.mni.compilerbau.types.*;
+import de.thm.mni.compilerbau.types.ArrayType;
+import de.thm.mni.compilerbau.utils.SplError;
+import de.thm.mni.compilerbau.phases._04a_tablebuild.TableBuilder.*;
+import java.util.ArrayList;
+
+import static de.thm.mni.compilerbau.phases._04a_tablebuild.TableBuilder.printSymbolTableAtEndOfProcedure;
+
+public class SymbolTableVisitor extends DoNothingVisitor {
+
+    private Identifier typeName;
+    private SymbolTable globalTable;
+    private SymbolTable lokaleTable;
+    private ArrayList<ParameterType> parameterTypeList;
+    private CommandLineOptions options;
+
+    public SymbolTableVisitor(SymbolTable globalTable, CommandLineOptions options ) {
+
+        this.globalTable = globalTable;
+        this.options = options;
+    }
+
+    @Override
+    public void visit(Program program) {
+        program.definitions.stream().forEach(dec -> {
+            if (dec instanceof TypeDefinition || dec instanceof ProcedureDefinition) {
+                dec.accept(this);
+            }
+        });
+        var main = globalTable.lookup(new Identifier("main"), SplError.MainIsMissing());
+        if (!(main instanceof ProcedureEntry proEntry)){
+            throw SplError.MainIsNotAProcedure();
+        }
+        if (!(proEntry.parameterTypes.isEmpty())){
+            throw SplError.MainMustNotHaveParameters();
+        }
+    }
+
+    @Override
+    public void visit(TypeDefinition typeDefinition) {
+        globalTable.enter(typeDefinition.name, new TypeEntry(getType(globalTable, typeDefinition.typeExpression)));
+        if (typeDefinition.typeExpression instanceof NamedTypeExpression
+                || typeDefinition.typeExpression instanceof ArrayTypeExpression) {
+            typeDefinition.typeExpression.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(ArrayTypeExpression arrayTypeExpression) {
+        arrayTypeExpression.baseType.accept(this);
+
+    }
+
+    @Override
+    public void visit(ProcedureDefinition procedureDefinition) {
+        lokaleTable = new SymbolTable(globalTable);
+        parameterTypeList = new ArrayList<>();
+        procedureDefinition.variables.forEach(varDef -> {
+            varDef.accept(this);
+        });
+        procedureDefinition.parameters.forEach(parameterDefinition -> {
+            parameterDefinition.accept(this);
+        });
+        ProcedureEntry procedureEntry = new ProcedureEntry(lokaleTable, parameterTypeList);
+        globalTable.enter(
+                procedureDefinition.name,
+                procedureEntry,
+                SplError.RedefinitionOfIdentifier(procedureDefinition.position, procedureDefinition.name)
+        );
+        if (options.phaseOption == CommandLineOptions.PhaseOption.TABLES){
+            printSymbolTableAtEndOfProcedure(procedureDefinition.name, procedureEntry);
+        }
+
+    }
+
+    @Override
+    public void visit(VariableDefinition variableDefinition) {
+        lokaleTable.enter(
+                variableDefinition.name,
+                new VariableEntry(getType(lokaleTable, variableDefinition.typeExpression), false),
+                SplError.RedefinitionOfIdentifier(variableDefinition.position,variableDefinition.name)
+        );
+
+    }
+
+    @Override
+    public void visit(ParameterDefinition parameterDefinition) {
+        parameterTypeList.add(
+                new ParameterType(getType(lokaleTable, parameterDefinition),
+                        parameterDefinition.isReference)
+        );
+
+
+    }
+
+    public Type getType(SymbolTable table, TypeExpression typeExpression) {
+        switch (typeExpression) {
+            case NamedTypeExpression namedTypeExpression -> {
+                var entry = table.lookup(namedTypeExpression.name, SplError.UndefinedIdentifier(namedTypeExpression.position, namedTypeExpression.name));
+                if (entry instanceof TypeEntry typeEntry) {
+                    return typeEntry.type;
+                } else {
+                    throw SplError.NotAType(namedTypeExpression.position, namedTypeExpression.name);
+                }
+            }
+            case ArrayTypeExpression arrayTypeExpression -> {
+                return new ArrayType(getType(table, arrayTypeExpression.baseType), arrayTypeExpression.arraySize);
+            }
+        }
+    }
+    public Type getType(SymbolTable table, ParameterDefinition parameterDefinition) {
+      var parameterType = getType(table, parameterDefinition.typeExpression);
+      VariableEntry variableEntry = new VariableEntry(parameterType,parameterDefinition.isReference);
+        table.enter(parameterDefinition.name,
+                    variableEntry,
+                    SplError.RedefinitionOfIdentifier(parameterDefinition.position,parameterDefinition.name)
+        );
+        //TODO handel Eroor aber welchen ???
+        return parameterType;
+    }
+
+    public SymbolTable getGlobalTable() {
+        return globalTable;
+    }
+
+    public SymbolTable getLokaleTable() {
+        return lokaleTable;
+    }
+}
--- spl-compiler-skeleton/java/src/main/java/de/thm/mni/compilerbau/phases/_04a_tablebuild/TableBuilder.java
+++ spl-compiler-skeleton/java/src/main/java/de/thm/mni/compilerbau/phases/_04a_tablebuild/TableBuilder.java
@@ -1,15 +1,15 @@
 package de.thm.mni.compilerbau.phases._04a_tablebuild;
 
 import de.thm.mni.compilerbau.CommandLineOptions;
-import de.thm.mni.compilerbau.absyn.Expression;
-import de.thm.mni.compilerbau.absyn.Program;
-import de.thm.mni.compilerbau.absyn.TypeExpression;
-import de.thm.mni.compilerbau.absyn.Variable;
+import de.thm.mni.compilerbau.absyn.*;
 import de.thm.mni.compilerbau.table.Identifier;
 import de.thm.mni.compilerbau.table.ProcedureEntry;
 import de.thm.mni.compilerbau.table.SymbolTable;
+import de.thm.mni.compilerbau.table.TypeEntry;
 import de.thm.mni.compilerbau.types.Type;
 import de.thm.mni.compilerbau.utils.NotImplemented;
+import de.thm.mni.compilerbau.absyn.visitor.SymbolTableVisitor;
+import de.thm.mni.compilerbau.utils.SplError;
 
 /**
  * This class is used to create and populate a {@link SymbolTable} containing entries for every symbol in the currently
@@ -28,9 +28,20 @@ public class TableBuilder {
 
     public SymbolTable buildSymbolTable(Program program) {
         //TODO (assignment 4a): Initialize a symbol table with all predefined symbols and fill it with user-defined symbols
-
-        throw new NotImplemented();
+//
+        var globalTable = TableInitializer.initializeGlobalTable();
+        SymbolTableVisitor symbolTableVisitor = new SymbolTableVisitor(globalTable, options);
+        program.accept(symbolTableVisitor);
+        var main = globalTable.lookup(new Identifier("main"), SplError.MainIsMissing());
+        if (!(main instanceof ProcedureEntry proEntry)){
+            throw SplError.MainIsNotAProcedure();
+        }
+        if (!(proEntry.parameterTypes.isEmpty())){
+            throw SplError.MainMustNotHaveParameters();
+        }
+        return symbolTableVisitor.getGlobalTable();
     }
+
 
     /**
      * Prints the local symbol table of a procedure together with a heading-line
@@ -39,7 +50,7 @@ public class TableBuilder {
      * @param name  The name of the procedure
      * @param entry The entry of the procedure to print
      */
-    static void printSymbolTableAtEndOfProcedure(Identifier name, ProcedureEntry entry) {
+    public static void printSymbolTableAtEndOfProcedure(Identifier name, ProcedureEntry entry) {
         System.out.format("Symbol table at end of procedure '%s':\n", name);
         System.out.println(entry.localTable.toString());
     }
